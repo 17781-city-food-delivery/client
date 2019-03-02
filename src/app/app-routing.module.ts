a9e98f50c5eb48e230f5b967ecbcbbc6a6d4e3c0
wjt <jtwang.me@gmail.com>
2019-03-02 18:47:14 -0500

    login logic

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
-import {Tab1PageModule} from "./tab1/tab1.module";
+
 
 const routes: Routes = [
-  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
+  { path: '', loadChildren: './signin/signin.module#SigninPageModule'},
+  { path: 'main', loadChildren: './tabs/tabs.module#TabsPageModule' },
   { path: 'menu', loadChildren: './menu/menu.module#MenuPageModule' },
   { path: 'dish-detail', loadChildren: './dish-detail/dish-detail.module#DishDetailPageModule' },
   { path: 'cart', loadChildren: './cart/cart.module#CartPageModule' },
