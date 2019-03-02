d9542f2a32ec8a73b46f2040685826a9a92e3e8c
wjt <jtwang.me@gmail.com>
2019-03-02 15:42:54 -0500

    add signin, signup pages, update profile page such that it can link to other pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,9 @@ const routes: Routes = [
   { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
   { path: 'menu', loadChildren: './menu/menu.module#MenuPageModule' },
   { path: 'dish-detail', loadChildren: './dish-detail/dish-detail.module#DishDetailPageModule' },
-  { path: 'cart', loadChildren: './cart/cart.module#CartPageModule' }
+  { path: 'cart', loadChildren: './cart/cart.module#CartPageModule' },
+  { path: 'signup', loadChildren: './signup/signup.module#SignupPageModule' },
+  { path: 'signin', loadChildren: './signin/signin.module#SigninPageModule' }
 ];
 @NgModule({
   imports: [
--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -6,32 +6,38 @@ import { Component } from '@angular/core';
   styleUrls: ['tab3.page.scss']
 })
 export class Tab3Page {
-  private profile_items: ({ icon: string; label: string })[];
+  private profile_items: ({ icon: string; label: string; link: string })[];
   constructor() {
     this.profile_items = [
       {
         'icon': 'clipboard',
-        'label': 'Edit Profile'
+        'label': 'Edit Profile',
+        'link': null,
       },
       {
         'icon': 'car',
-        'label': 'Track Order'
+        'label': 'Track Order',
+        'link': null,
       },
       {
         'icon': 'card',
-        'label': 'My Credits'
+        'label': 'My Credits',
+        'link': null,
       },
       {
         'icon': 'barcode',
-        'label': 'My QRCode'
+        'label': 'My QRCode',
+        'link': null,
       },
       {
         'icon': 'contacts',
-        'label': 'Restaurants! Partner with us'
+        'label': 'Restaurants! Partner with us',
+        'link': null,
       },
       {
         'icon': 'log-out',
-        'label': 'Log out'
+        'label': 'Log out',
+        'link': '/signin'
       }
     ];
   }
