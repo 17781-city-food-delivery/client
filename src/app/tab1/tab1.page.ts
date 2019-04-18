20a742c158c7dbc8146363aac043d7fcb6dff5f6
ciciyu226 <ciciyu226@gmail.com>
2019-04-18 14:15:56 -0400

    Display restaurants dynamically on tab1

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,17 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import {MenuPage} from '../menu/menu.page';
-import {NavController} from '@ionic/angular';
+// import {NavController} from '@ionic/angular';
+import { Router } from '@angular/router';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
   slideOpts = {initialSlide: 0, slidesPerView: 1, autoplay: true};
   menuPage = MenuPage;
+  restaurants: Array<object>=[];
+  // objectKeys = Object.keys;
 
-  constructor(public navCtrl: NavController) {}
-  
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    //load all restaurants
+    let rootRef = firebase.database().ref();
+    rootRef.child('restaurants').once('value').then(
+      snapshot => {
+        // this.restaurants = snapshot.val();
+        console.log(this.restaurants);
+        snapshot.forEach(item => {
+          console.log(item.key + ":" + JSON.stringify(item.val()))
+          let restaurant = {
+            key: item.key,
+            value: item.val()
+          }
+          this.restaurants.push(restaurant);
+        })
+      }
+    );
+  }
+
+  //on every load, grey out the unavaliable restaurants
 }
