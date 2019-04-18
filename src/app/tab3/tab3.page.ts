26ad86154d05a18b7c1558964f2143b2d558bb1d
Jasper Hsieh <jasperhsieh666@gmail.com>
2019-04-18 12:28:54 -0400

    Fix build error due to 'constants module not found' and OnInit not imported

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthenticationService } from '../services/authentication.service';
 import * as firebase from 'firebase';
