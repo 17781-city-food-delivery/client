6ba32886837240b8d390819194c7288a9d143ddb
Jasper Hsieh <jasperhsieh666@gmail.com>
2019-04-19 21:25:19 -0400

    Fix Cannot find module for restaurant-add-meal.page

--- src/app/restaurant-meals/restaurant-meals.page.ts
+++ src/app/restaurant-meals/restaurant-meals.page.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import * as firebase from 'firebase';
-import {RestaurantAddMealPage} from '../restaurant-add-meal/restaurant-add-meal.page';
 import {NavController} from '@ionic/angular';
 
 @Component({
